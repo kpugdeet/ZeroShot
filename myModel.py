2b64cac9425fe1a8fb3f9bba639012e8a6f33d5c
kpugdeet <krittaphat.pug@gmail.com>
2018-01-16 10:01:36 -0500

    Update

--- myModel.py
+++ myModel.py
@@ -4,102 +4,154 @@ import globalV
 from darknet import *
 
 
-class MyModel (object):
+class myModel (object):
     def __init__(self):
         self.x = tf.placeholder(tf.float32, name='inputImage', shape=[None, globalV.FLAGS.height, globalV.FLAGS.width, 3])
         processX = tf.subtract(tf.multiply(tf.divide(self.x, 255), 2.0), 1.0)
         self.y = tf.placeholder(tf.int64, name='outputClass', shape=[None])
+        self.attY = tf.placeholder(tf.float32, name='outputAtt', shape=[None, globalV.FLAGS.numAtt])
+        self.transformW = tf.placeholder(tf.float32, name='transformW', shape=[globalV.FLAGS.numAtt, None])
         self.isTraining = tf.placeholder(tf.bool)
-        core_net = darknet19(processX, is_training=self.isTraining)
+        self.core_net = darknet19(processX, is_training=self.isTraining)
 
-        # Last layer network
-        w = tf.get_variable(name='weight', shape=[1000, globalV.FLAGS.numClass], dtype=tf.float32)
-        self.b = tf.get_variable(name='bias', shape=[globalV.FLAGS.numClass], dtype=tf.float32)
+        # Attribute Layer
+        wAtt = tf.get_variable(name='attWeight', shape=[1000, globalV.FLAGS.numAtt], dtype=tf.float32)
+        bAtt = tf.get_variable(name='attBias', shape=[globalV.FLAGS.numAtt], dtype=tf.float32)
+        self.predAtt = tf.sigmoid(tf.add(tf.matmul(self.core_net, wAtt), bAtt))
+        attLoss = tf.losses.absolute_difference(self.predAtt, self.attY)
+        meanAttLoss = tf.reduce_mean(attLoss)
 
-        # Softmax loss function and Adam optimizer
-        predictY = tf.add(tf.matmul(core_net, w), self.b)
-        self.meanLoss = tf.reduce_mean(tf.losses.softmax_cross_entropy(tf.one_hot(self.y, globalV.FLAGS.numClass), logits=predictY))
-        optimizer = tf.train.AdamOptimizer(globalV.FLAGS.lr)
-        self.trainStep = optimizer.minimize(self.meanLoss)
+        # Class Loss at the last layer
+        predClass = tf.matmul(self.predAtt, self.transformW)
+        meanLastLoss = tf.reduce_mean(tf.losses.softmax_cross_entropy(tf.one_hot(self.y, globalV.FLAGS.numClass), logits=predClass))
 
-        # Predict the output
-        correctPrediction = tf.equal(tf.argmax(predictY, 1), self.y)
-        self.predictIndex = tf.argmax(predictY, 1)
+        # Predict the output for darknet
+        self.predictIndex = tf.argmax(predClass, 1)
+
+
+        correctPrediction = tf.equal(tf.argmax(predClass, 1), self.y)
         self.accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
+
+        self.finalLoss = meanAttLoss
+
+        # Batch Normalization fixed
+        updateOps = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(updateOps):
+            self.attTrainStep = tf.train.AdamOptimizer(globalV.FLAGS.lr).minimize(self.finalLoss)
 
         # Log all output
         self.averageL = tf.placeholder(tf.float32)
-        lossSummary = tf.summary.scalar("averageLoss", self.averageL)
+        tf.summary.scalar("averageLoss", self.averageL)
         self.averageA = tf.placeholder(tf.float32)
-        accSummary = tf.summary.scalar("averageAcc", self.averageA)
+        tf.summary.scalar("averageAcc", self.averageA)
 
         # Merge all log
         self.merged = tf.summary.merge_all()
 
         # Initialize session
-        self.sess = tf.Session()
+        tfConfig = tf.ConfigProto(allow_soft_placement=True)
+        tfConfig.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=tfConfig)
         self.sess.run(tf.global_variables_initializer())
-        self.saver = tf.train.Saver()
+        self.saver = tf.train.Saver(max_to_keep=None)
+
+        # Log directory
         if globalV.FLAGS.NEW == 1:
-            if tf.gfile.Exists(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/logs'):
-                tf.gfile.DeleteRecursively(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/logs')
-            tf.gfile.MakeDirs(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/logs')
-        self.trainWriter = tf.summary.FileWriter(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/logs/train', self.sess.graph)
-        self.testWriter = tf.summary.FileWriter(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/logs/test')
+            if tf.gfile.Exists(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/logs'):
+                tf.gfile.DeleteRecursively(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/logs')
+            tf.gfile.MakeDirs(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/logs')
+        self.trainWriter = tf.summary.FileWriter(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/logs/train', self.sess.graph)
+        self.testWriter = tf.summary.FileWriter(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/logs/test')
 
         # Start point and log
-        self.start = 1
-        self.check = 10
+        self.Start = 1
+        self.Check = 10
+        if globalV.FLAGS.NEW == 0:
+            self.restoreModel()
+        else:
+            variableToRestore = tf.contrib.slim.get_variables_to_restore(exclude=['attBias', 'attWeight'])
+            saverDarknet = tf.train.Saver(variableToRestore)
+            saverDarknet.restore(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/darknet/model/model.ckpt')
 
 
     def restoreModel(self):
-        self.saver.restore(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/model/model.ckpt')
-        npzFile = np.load(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/model/checkpoint.npz')
-        self.start = npzFile['start']
-        self.check = npzFile['check']
+        self.saver.restore(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/model/model.ckpt')
+        npzFile = np.load(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/model/checkpoint.npz')
+        self.Start = npzFile['Start']
+        self.Check = npzFile['Check']
 
 
-    def trainDarknet(self, trainX, trainY):
-        if globalV.FLAGS.NEW == 0:
-            self.restoreModel()
-        for i in range(self.start, globalV.FLAGS.maxSteps):
+    def trainAtt(self, trainX, trainY, trainYAtt, valX, valY, valYAtt, transformW):
+        print(trainX.shape, trainY.shape, trainYAtt.shape, valX.shape, valY.shape, valYAtt.shape, transformW.shape)
+        for i in range(self.Start, globalV.FLAGS.maxSteps + 1):
             print('Loop {0}/{1}'.format(i, globalV.FLAGS.maxSteps))
             # Train
             s = np.arange(trainX.shape[0])
             np.random.shuffle(s)
             tmpX = trainX[s]
             tmpY = trainY[s]
+            tmpAttY = trainYAtt[s]
             losses = []
             accuracies = []
             for j in range(0, trainX.shape[0], globalV.FLAGS.batchSize):
                 xBatch = tmpX[j:j + globalV.FLAGS.batchSize]
                 yBatch = tmpY[j:j + globalV.FLAGS.batchSize]
-                trainLoss, trainAccuracy, _ = self.sess.run([self.meanLoss, self.accuracy, self.trainStep], feed_dict={self.x: xBatch, self.y: yBatch, self.isTraining: 1})
+                attYBatch = tmpAttY[j:j + globalV.FLAGS.batchSize]
+                trainLoss, trainAccuracy,  _ = self.sess.run([self.finalLoss, self.accuracy ,self.attTrainStep], feed_dict={self.x: xBatch, self.y: yBatch, self.attY: attYBatch,
+                                                                                                                            self.transformW: transformW, self.isTraining: 1})
                 losses.append(trainLoss)
                 accuracies.append(trainAccuracy)
             feed = {self.averageL: sum(losses) / len(losses), self.averageA: sum(accuracies) / len(accuracies)}
             summary = self.sess.run(self.merged, feed_dict=feed)
             self.trainWriter.add_summary(summary, i)
-            # # Validation
-            # losses = []
-            # accuracies = []
-            # for j in range(0, valX.shape[0], globalV.FLAGS.batchSize):
-            #     xBatch = valX[j:j + globalV.FLAGS.batchSize]
-            #     yBatch = valY[j:j + globalV.FLAGS.batchSize]
-            #     valLoss, valAccuracy, _ = sess.run([meanLoss, accuracy, trainStep],feed_dict={x: xBatch, y: yBatch, isTraining: 0})
-            #     losses.append(valLoss)
-            #     accuracies.append(valAccuracy)
-            #
-            # feed = {averageL: sum(losses) / len(losses), averageA: sum(accuracies) / len(accuracies)}
-            # summary = sess.run(merged, feed_dict=feed)
-            # testWriter.add_summary(summary, i)
-            if i % self.check == 0:
-                savePath = self.saver.save(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/model/model.ckpt')
-                np.savez(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/model/checkpoint.npz', start=i, check=self.check)
-                print('Model saved in file: {0}'.format(savePath))
-                if i / self.check == 10:
-                    self.check *= 10
 
-        # Close Log
-        self.trainWriter.close()
-        self.testWriter.close()
+            # Validation
+            losses = []
+            accuracies = []
+            for j in range(0, valX.shape[0], globalV.FLAGS.batchSize):
+                xBatch = valX[j:j + globalV.FLAGS.batchSize]
+                yBatch = valY[j:j + globalV.FLAGS.batchSize]
+                attYBatch = valYAtt[j:j + globalV.FLAGS.batchSize]
+                valLoss, valAccuracy, _ = self.sess.run([self.finalLoss, self.accuracy, self.attTrainStep], feed_dict={self.x: xBatch, self.y: yBatch, self.attY: attYBatch,
+                                                                                                                       self.transformW: transformW, self.isTraining: 0})
+                losses.append(valLoss)
+                accuracies.append(valAccuracy)
+            feed = {self.averageL: sum(losses) / len(losses), self.averageA: sum(accuracies) / len(accuracies)}
+            summary = self.sess.run(self.merged, feed_dict=feed)
+            self.testWriter.add_summary(summary, i)
+
+            if i % self.Check == 0:
+                savePath = self.saver.save(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/model/model.ckpt',global_step=i)
+                print('Model saved in file: {0}'.format(savePath))
+                if i / self.Check == 10:
+                    self.Check *= 10
+
+            # Save State
+            np.savez(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/model/checkpoint.npz', Start=i+1, Check=self.Check)
+            self.saver.save(self.sess, globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/myModel/model/model.ckpt')
+
+    def checkCoreNet(self, trainX):
+        return self.sess.run(self.core_net, feed_dict={self.x: trainX, self.isTraining: 0})
+
+    def predict(self, trainX, transformW):
+        return self.sess.run(self.predictIndex, feed_dict={self.x: trainX, self.transformW: transformW, self.isTraining: 0})
+
+    def calAcc(self, trainX, trainY, transformW):
+        accuracies = []
+        for j in range(0, trainX.shape[0], globalV.FLAGS.batchSize):
+            xbatch = trainX[j:j + globalV.FLAGS.batchSize]
+            yBatch = trainY[j:j + globalV.FLAGS.batchSize]
+            acc = self.sess.run(self.accuracy, feed_dict={self.x: xbatch, self.y: yBatch, self.transformW: transformW, self.isTraining: 0})
+            accuracies.append(acc)
+        return sum(accuracies) / len(accuracies)
+
+    def predAtts(self, trainX):
+        atts = None
+        for j in range(0, trainX.shape[0], globalV.FLAGS.batchSize):
+            xBatch = trainX[j:j + globalV.FLAGS.batchSize]
+            att = self.sess.run(self.predAtt, feed_dict={self.x: xBatch, self.isTraining: 0})
+            if atts is None:
+                atts = att
+            else:
+                atts = np.concatenate((atts, att), axis=0)
+        return np.array(atts)
--- run.py
+++ run.py
@@ -3,9 +3,14 @@ import globalV
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
 import numpy as np
 from loadData import loadData
-from myModel import MyModel
+from myModel import myModel
+from darknetModel import darknetModel
 from vec2att import vec2att
 import tensorflow as tf
 
@@ -18,35 +23,140 @@ if __name__ == "__main__":
     parser.add_argument('--SUNPATH', type=str, default='SUN/SUNAttributeDB/', help='Path for SUN dataset')
     parser.add_argument('--APYPATH', type=str, default='APY/attribute_data/', help='Path for APY dataset')
     parser.add_argument('--GOOGLE', type=str, default='GoogleNews-vectors-negative300.bin', help='Path for google Word2Vec model')
-    parser.add_argument('--KEY', type=str, default='CUB',help='Choose dataset (AWA2, CUB, SUN, APY)')
+    parser.add_argument('--KEY', type=str, default='APY',help='Choose dataset (AWA2, CUB, SUN, APY)')
     parser.add_argument('--maxSteps', type=int, default=1000, help='Number of steps to run trainer.')
     parser.add_argument('--lr', type=float, default=5e-4, help='Initial learning rate')
     parser.add_argument('--reg', type=float, default=5e4, help='Initial regularization')
     parser.add_argument('--width', type=int, default=300, help='Width')
     parser.add_argument('--height', type=int, default=300, help='Height')
-    parser.add_argument('--numClass', type=int, default=100, help='Number of class')
-    parser.add_argument('--batchSize', type=int, default=64, help='Number of batch size')
+    parser.add_argument('--numClass', type=int, default=15, help='Number of class')
+    parser.add_argument('--numAtt', type=int, default=64, help='Dimension of Attribute')
+    parser.add_argument('--batchSize', type=int, default=32, help='Number of batch size')
     parser.add_argument('--NEW', type=int, default=1, help='Train from scratch')
     globalV.FLAGS, _ = parser.parse_known_args()
 
     print('Load Data')
     (trainClass, trainAtt, trainVec, trainX, trainY, trainYAtt), (valClass, valAtt, valVec, valX, valY, valYAtt), (testClass, testAtt, testVec, testX, testY, testYAtt) = loadData.getData()
-    if globalV.FLAGS.KEY != 'SUN' and globalV.FLAGS.KEY != 'APY':
-        print(len(trainClass), trainAtt.shape, trainVec.shape, trainX.shape, trainY.shape)
-        print(len(valClass), valAtt.shape, valVec.shape, valX.shape, valY.shape)
-        print(len(testClass), testAtt.shape, testVec.shape, testX.shape, testY.shape)
-    else:
+
+    if globalV.FLAGS.KEY == 'SUN' or globalV.FLAGS.KEY == 'APY':
         print(len(trainClass), trainAtt.shape, trainVec.shape, trainX.shape, trainY.shape, trainYAtt.shape)
         print(len(valClass), valAtt.shape, valVec.shape, valX.shape, valY.shape, valYAtt.shape)
         print(len(testClass), testAtt.shape, testVec.shape, testX.shape, testY.shape, testYAtt.shape)
+    else:
+        print(len(trainClass), trainAtt.shape, trainVec.shape, trainX.shape, trainY.shape)
+        print(len(valClass), valAtt.shape, valVec.shape, valX.shape, valY.shape)
+        print(len(testClass), testAtt.shape, testVec.shape, testX.shape, testY.shape)
+
+    # def printClassName(pos):
+    #     if pos < 15:
+    #         return trainClass[pos]
+    #     elif pos < 20:
+    #         return valClass[pos-15]
+    #     else:
+    #         return testClass[pos-20]
+    #
+    # concatAtt = np.concatenate((trainAtt, valAtt, testAtt), axis=0)
+    # concatOut = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31])
+    # checkAtt = concatAtt
+    # for i in range(checkAtt.shape[0]):
+    #     for j in range(i + 1, checkAtt.shape[0]):
+    #         if np.array_equal(checkAtt[i], checkAtt[j]):
+    #             print('{0} {1}: {2} {3}'.format(i, printClassName(i), j, printClassName(j)))
+    #             print(checkAtt[i])
+
+    # Paper Way
+    def calAccPaper(yAtt, att, y, name='Train'):
+        yAtt = np.expand_dims(yAtt, axis=1)
+        tmpOut = np.divide(yAtt, att, out=np.ones((yAtt.shape[0], att.shape[0], att.shape[1])), where=att!=0)
+        tmpOut = np.prod(tmpOut, axis=2)
+        acc = np.mean(np.equal(np.argmax(tmpOut, 1), y))
+        print(name+' acc: {0}'.format(acc))
+
+    # Dot Prod Way
+    def calAccDotProd(yAtt, att, y, name='Train'):
+        tmpOut = np.dot(yAtt, att.T)
+        acc = np.mean(np.equal(np.argmax(tmpOut, 1), y))
+        print(name + ' acc: {0}'.format(acc))
+
+    # Dot Prod Way with Avg
+    def calAccDotProdAvg(yAtt, att, y, name='Train'):
+        tmpOut = np.dot(yAtt, att.T)/np.sum(att.T, axis=0)
+        acc = np.mean(np.equal(np.argmax(tmpOut, 1), y))
+        print(name + ' acc: {0}'.format(acc))
+
+    def callAll (yAtt, att, y, name='Train'):
+        calAccPaper(yAtt, att, y, name)
+        calAccDotProd(yAtt, att, y, name)
+        calAccDotProdAvg(yAtt, att, y, name)
+
+    callAll(trainYAtt, trainAtt, trainY, 'Train')
+    callAll(valYAtt, valAtt, valY, 'Val')
+    callAll(testYAtt, testAtt, testY, 'Test')
+    print()
 
     print('Train CNN')
-    model = MyModel()
-    model.trainDarknet(trainX, trainY)
-    # model.trainDarknetFromCheckPoint(trainX, trainY)
+    s = np.arange(trainX.shape[0])
+    dividePoint = int(trainX.shape[0]*0.7)
+    np.random.shuffle(s)
+    tmpX = trainX[s]
+    tmpY = trainY[s]
+    # tmpYAtt = trainYAtt[s]
+    # Each class has same attribute
+    trainYAtt2 = list()
+    for i in range(trainY.shape[0]):
+        trainYAtt2.append(trainAtt[trainY[i]])
+    tmpYAtt = np.array(trainYAtt2)[s]
 
+    # darknet = darknetModel()
+    # # darknet.trainDarknet(tmpX[:5], tmpY[:5], tmpX[5:10], tmpY[5:10])
+    # # print(darknet.checkCoreNet(trainX[:2]))
+    # darknet.trainDarknet(tmpX[:dividePoint], tmpY[:dividePoint], tmpX[dividePoint:], tmpY[dividePoint:])
 
+    my = myModel()
+    # print(my.checkCoreNet(trainX[:2]))
+    # my.trainAtt(tmpX[:5], tmpYAtt[:5], tmpX[5:10], tmpYAtt[5:10])
+    my.trainAtt(tmpX[:dividePoint], tmpY[:dividePoint], tmpYAtt[:dividePoint], tmpX[dividePoint:], tmpY[dividePoint:], tmpYAtt[dividePoint:], np.ceil(trainAtt.T))
 
+    # print(my.predict(testX[:50], np.ceil(testAtt.T)))
+    # print(testY[:50])
+    # callAll(my.predAtts(trainX), trainAtt, trainY, 'Train')
+    # callAll(my.predAtts(valX), valAtt, valY, 'Val')
+    # callAll(my.predAtts(testX), testAtt, testY, 'Test')
+    # print()
+
+    # # Attribute itself
+    # callAll(trainAtt, trainAtt, np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), 'Train')
+    # callAll(valAtt, valAtt, np.array([0, 1, 2, 3, 4]), 'Val')
+    # callAll(testAtt, testAtt, np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), 'Test')
+    # print()
+
+    # tmpPredClass = np.dot(testYAtt[:20], np.ceil(testAtt.T))
+    # tmpPredictIndex = np.argmax(tmpPredClass, 1)
+    # # predictLabel = my.predict(testX[:10], np.ceil(testAtt.T))
+    # print('Save image')
+    # plt.figure(figsize=(6, 24))
+    # for i in range(20):
+    #     plt.subplot(10, 2, i+1)
+    #     plt.title('P: '+testClass[tmpPredictIndex[i]]+'\nA: '+testClass[testY[i]])
+    #     plt.imshow(testX[i], aspect='auto')
+    #     plt.axis('off')
+    # plt.tight_layout()
+    # plt.savefig('Test.png')
+    # plt.clf()
+
+    # summary_op = tf.summary.image("plot", tmpX)
+    # with tf.Session() as sess:
+    #     summary = sess.run(summary_op)
+    #     writer = tf.summary.FileWriter(globalV.FLAGS.BASEDIR + globalV.FLAGS.KEY + '/imageLogs/')
+    #     writer.add_summary(summary)
+    #     writer.close()
+
+    # s = np.arange(trainX.shape[0])
+    # np.random.shuffle(s)
+    # tmpX = trainX[s]
+    # tmpY = trainY[s]
+    # print(model.predict(tmpX[:10]))
+    # print(tmpY[:10])
 
 
 
